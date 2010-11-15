829986697d9dc1d4d62c90bd5757ebbd19de430f
devangel <none@none>
2010-11-15 15:47:20 -0800

    Initial commit

---
+++ dinosting.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+# Module:   dinosting.py
+# Created:  10 November 2010
+# Author:   Dennis Evangelista
+# Version:  0.1
+# License:  GPLv3
+
+'''
+This module provides a serial connection abstraction layer for talking to
+the wind tunnel and sting for 4115 VLSB Microraptor gui experiments in
+the Department of Integrative Biology, UC Berkeley.
+'''
+
+import serial
+from serial.serialutil import SerialException
+
+class DinoSting:
+    '''class DinoSting is an instance of a wind tunnel and sting.'''
+
+    # Static attributes for talking to Arduino.
+    # Must match definitions in DinoStingFan.pdf Arduino code.
+    START = chr(255) # Start byte code
+    SERVO = chr(1)   # Servo byte code
+    FAN = chr(2)     # Fan byte code
+    FANON = chr(1)   # Turn fan on
+    FANOFF = chr(0)  # Turn fan off
+
+    def __init__(self,port='COM4',mount_angle=0):
+        ''' DinoSting constructor takes port (default COM4) and mount angle.'''
+        self.mount_angle = mount_angle
+        try:
+            self.ser = serial.Serial(port, timeout = 1)
+            print 'Connected on {0}.'.format(port)
+        except SerialException:
+            print 'Could not connect with Arduino on {0}, is it plugged in?'.format(port)
+        # other possibilities are
+        # port = '/dev/tty.usbserial-A600ehci' for mac
+        # port = '/dev/ttyUSB0' for linux
+
+
+
+    def move(self,angle=0):
+        '''Moves the servo sting to the supplied angle.  Angle must be
+between 0 and 180 degrees.'''
+        if (0 <= (angle-self.mount_angle) <= 180):
+            self.ser.write(DinoSting.START)
+            self.ser.write(DinoSting.SERVO)
+            self.ser.write(chr(angle-self.mount_angle))
+            print 'Sting trained to %d adjusted or %d raw.' % (angle,(angle-self.mount_angle))
+        else:
+            print 'Servo angle plus mount angle must be an integer between 0 and 180.'
+
+
+
+    def fan(self,state=0):
+        '''Sets the wind tunnel on or off'''
+        if (state == 1):
+            print 'Fan running.'
+            self.ser.write(DinoSting.START)
+            self.ser.write(DinoSting.FAN)
+            self.ser.write(DinoSting.FANON)
+        else:
+            print 'Fan secured.'
+            self.ser.write(DinoSting.START)
+            self.ser.write(DinoSting.FAN)
+            self.ser.write(DinoSting.FANOFF)
+
+
+    def gohome(self):
+        print 'Going home...'
+        self.move(0)
+        self.fan(0)
+        print 'Wind tunnel home.'
+        
+
+
+    def shutdown(self):
+        ''' Zeros servo position and turns off fan'''
+        print 'Shutting down...'
+        self.move(0)
+        self.fan(0)
+        print 'Wind tunnel secured.'
+
+
+
+
+    def __del__(self):
+        ''' DinoSting destructor shuts down port and secures serial port.'''
+        self.shutdown()
+        self.ser.close()
+        print 'DinoSting object garbage collected.'
+        
+ 
+        
+
+        
